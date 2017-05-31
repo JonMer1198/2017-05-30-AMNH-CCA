0d6b21326fa7cfb7aa8804e0b2a4e67d79f31735
Jonathan <jonathan.mercedes@lc.cuny.edu>
2017-05-31 11:24:03 -0400

    We added a new python script. For now

---
+++ python_practice/practice_script.py
@@ -0,0 +1,5 @@
+'''
+This script contains an example class that we will write. The class will be called algebra, and it will contain some simple methods to do some algebraic manipulation.
+
+The scropt will end with 
+'''

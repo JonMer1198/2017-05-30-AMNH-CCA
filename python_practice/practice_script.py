402ec118ca4cf756c70a34beb79e3d7d0a1d1d85
Jonathan <jonathan.mercedes@lc.cuny.edu>
2017-06-01 12:42:27 -0400

    Added some scripts into python_script.py

--- python_practice/practice_script.py
+++ python_practice/practice_script.py
@@ -5,6 +5,7 @@ The script will end with a sample way to call and use the class.
 '''
 
 sample_int = 5
+print (sample_int)
 
 class Algebra :
     def __init__(self,x,y) :
@@ -14,7 +15,7 @@ class Algebra :
         return self.x + self.y
     def subtract(self) :
         return self.x - self.y
-    def multply(self) :
+    def multiply(self) :
         return self.x * self.y
     def divide(self) :
         return self.x / self.y
@@ -22,3 +23,14 @@ class Algebra :
         return self.multiply() + self.subtract()
     def modify_x(self) :
         self.x = self.x * 2
+
+if __name__ == "__main__":
+    # Create instance of Algebra
+    myalg = Algebra(100, 150)
+    # Print some outputs using the Algebra class
+    print myalg.multiply()
+    myalg.modify_x()
+    print myalg.multiply()
+else :
+    print "name not equal to main"
+    print __name__
